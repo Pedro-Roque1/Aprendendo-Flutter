86ea2e347ebab9db16d5ecf1b565b7c48e1ae342
Pedro Roque Machado <pedro.machado@verticalti.com.br>
2021-11-16 08:01:37 -0300

    Gerador de palvaras aleatórias em inglês
    Alterado para gerar passar a usar um pacote externo de palavras
    aleatórias em inglês e trabalhar com os conceitos de widgets.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,22 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  const MyApp({Key? key}) : super(key: key);
-
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Bem vindo ao flutter',
+      title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Bem vindo ao Flutter'),
+          title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(child: Text("Olá Mundo")),
+        body: Center(child: RandomWords()),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
